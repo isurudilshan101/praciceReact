db396354d841e679e554acad007109c56e075740
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-01-06 02:00:14 +0530

    comment 2

--- practice/src/index.js
+++ practice/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const App =() =>{
-    return <div> hi there, </div>
+    return <div> hi isuru dilshan , this is my name !!!, </div>
 };
 
 ReactDom.render(<App/>,document.querySelector('#root'));
