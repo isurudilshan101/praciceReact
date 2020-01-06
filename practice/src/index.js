adc9d5eb768049aa210ed53337442eb0f4849fdb
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-01-07 01:21:34 +0530

    comment 2

--- practice/src/index.js
+++ practice/src/index.js
@@ -6,7 +6,10 @@ import CommentDetail from './CommentDetail';
 const App= () =>{
     return(
         <div className="ui container comments">
-           <CommentDetail/>
+           <CommentDetail author="isuru" timeAgo="yesterday 04:50pm" content="I like this"/>
+             <CommentDetail author="dilshan" timeAgo="Today 10:10am" content="Nice blog post"/>
+             <CommentDetail author="kamal" timeAgo="yesterday 05:19pm" content="I like the writing"/>
+          
         </div>
     );
 
