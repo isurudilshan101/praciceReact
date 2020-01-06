45bc324dda3b86395ded60eed2cdc178aab0055c
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-01-07 01:32:24 +0530

    comment 2

--- practice/src/CommentDetail.js
+++ practice/src/CommentDetail.js
@@ -5,7 +5,7 @@ const CommentDetail=props =>{
      return(
         <div className="comment">
         <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}/>
+            <img alt="avatar" src={props.avatar}/>
         </a>
         <div className="content">
             <a href="/" className="auther">
--- practice/src/index.js
+++ practice/src/index.js
@@ -6,9 +6,27 @@ import CommentDetail from './CommentDetail';
 const App= () =>{
     return(
         <div className="ui container comments">
-           <CommentDetail author="isuru" timeAgo="yesterday 04:50pm" content="I like this"/>
-             <CommentDetail author="dilshan" timeAgo="Today 10:10am" content="Nice blog post"/>
-             <CommentDetail author="kamal" timeAgo="yesterday 05:19pm" content="I like the writing"/>
+
+           <CommentDetail 
+            author="isuru"
+            timeAgo="yesterday 04:50pm"
+            content="I like this"
+            avatar={faker.image.avatar()}
+            />
+
+             <CommentDetail 
+             author="dilshan" 
+             timeAgo="Today 10:10am" 
+             content="Nice blog post"
+             avatar={faker.image.avatar()}
+             />
+
+             <CommentDetail
+              author="kamal" 
+              timeAgo="yesterday 05:19pm"
+              content="I like the writing"
+              avatar={faker.image.avatar()}
+              />
           
         </div>
     );
